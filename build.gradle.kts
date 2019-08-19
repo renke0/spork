18eda969dbd0d6fd24e6ae009f0fc23fb828eae6
Rob Enke <robsonenke@gmail.com>
2019-08-19 14:20:39 +0200

    Added initial gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+  groovy
+}
+
+group = "org.renke"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  compile("org.codehaus.groovy:groovy-all:2.3.11")
+}
